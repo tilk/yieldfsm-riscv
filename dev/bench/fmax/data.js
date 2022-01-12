08337b649bd3964a26b3b2ee10c776732d5ff1d2
github-action-benchmark <github@users.noreply.github.com>
2022-01-12 16:00:10 +0000

    add Fmax (nextpnr, ice40) (customBiggerIsBetter) benchmark result for 92cbdfa0cd95d84d6346cd8bacf57641ece177d7

--- dev/bench/fmax/data.js
+++ dev/bench/fmax/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1641993023447,
+  "lastUpdate": 1642003210132,
   "repoUrl": "https://github.com/tilk/yieldfsm-riscv",
   "entries": {
     "Fmax (nextpnr, ice40)": [
@@ -23,6 +23,45 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/tilk/yieldfsm-riscv/commit/e0e79046e2b2547480fa71b5bfdeafc708fc29b2"
         },
         "date": 1641993022658,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "explicitdp - fmax",
+            "value": 43.91,
+            "unit": "MHz"
+          },
+          {
+            "name": "explicit - fmax",
+            "value": 39.59,
+            "unit": "MHz"
+          },
+          {
+            "name": "yieldfsm - fmax",
+            "value": 35.65,
+            "unit": "MHz"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tilk@tilk.eu",
+            "name": "Marek Materzok",
+            "username": "tilk"
+          },
+          "committer": {
+            "email": "tilk@tilk.eu",
+            "name": "Marek Materzok",
+            "username": "tilk"
+          },
+          "distinct": true,
+          "id": "92cbdfa0cd95d84d6346cd8bacf57641ece177d7",
+          "message": "Performance data extraction via Perl script",
+          "timestamp": "2022-01-12T16:49:54+01:00",
+          "tree_id": "c45526de665e90376a472292d61de89d76a3bf74",
+          "url": "https://github.com/tilk/yieldfsm-riscv/commit/92cbdfa0cd95d84d6346cd8bacf57641ece177d7"
+        },
+        "date": 1642003209220,
         "tool": "customBiggerIsBetter",
         "benches": [
           {
