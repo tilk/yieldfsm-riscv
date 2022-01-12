e706e3cc19640bb23ce2def7fd2825d9e3080cf3
github-action-benchmark <github@users.noreply.github.com>
2022-01-12 13:10:24 +0000

    add LCs (nextpnr, ice40) (customSmallerIsBetter) benchmark result for e0e79046e2b2547480fa71b5bfdeafc708fc29b2

---
+++ dev/bench/lc/data.js
@@ -0,0 +1,47 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1641993024886,
+  "repoUrl": "https://github.com/tilk/yieldfsm-riscv",
+  "entries": {
+    "LCs (nextpnr, ice40)": [
+      {
+        "commit": {
+          "author": {
+            "email": "tilk@tilk.eu",
+            "name": "Marek Materzok",
+            "username": "tilk"
+          },
+          "committer": {
+            "email": "tilk@tilk.eu",
+            "name": "Marek Materzok",
+            "username": "tilk"
+          },
+          "distinct": true,
+          "id": "e0e79046e2b2547480fa71b5bfdeafc708fc29b2",
+          "message": "Performance data extraction via Perl script",
+          "timestamp": "2022-01-12T14:01:52+01:00",
+          "tree_id": "c45526de665e90376a472292d61de89d76a3bf74",
+          "url": "https://github.com/tilk/yieldfsm-riscv/commit/e0e79046e2b2547480fa71b5bfdeafc708fc29b2"
+        },
+        "date": 1641993024357,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "explicitdp - lc",
+            "value": 3790,
+            "unit": "LCs"
+          },
+          {
+            "name": "explicit - lc",
+            "value": 3726,
+            "unit": "LCs"
+          },
+          {
+            "name": "yieldfsm - lc",
+            "value": 5599,
+            "unit": "LCs"
+          }
+        ]
+      }
+    ]
+  }
+}
