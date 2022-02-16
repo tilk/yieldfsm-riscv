b52c5de444f0b12fb49eea677e45b5f6a8f567cf
github-action-benchmark <github@users.noreply.github.com>
2022-02-16 11:27:11 +0000

    add Fmax (nextpnr, ice40) (customBiggerIsBetter) benchmark result for 75b54d90070983d954a786265f29e89af2a3c338

--- dev/bench/fmax/data.js
+++ dev/bench/fmax/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1645009897463,
+  "lastUpdate": 1645010831534,
   "repoUrl": "https://github.com/tilk/yieldfsm-riscv",
   "entries": {
     "Fmax (nextpnr, ice40)": [
@@ -267,6 +267,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "explicitdp - fmax",
             "value": 43.91,
+            "unit": "MHz"
+          },
+          {
+            "name": "explicit - fmax",
+            "value": 39.59,
+            "unit": "MHz"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tilk@tilk.eu",
+            "name": "Marek Materzok",
+            "username": "tilk"
+          },
+          "committer": {
+            "email": "tilk@tilk.eu",
+            "name": "Marek Materzok",
+            "username": "tilk"
+          },
+          "distinct": true,
+          "id": "75b54d90070983d954a786265f29e89af2a3c338",
+          "message": "More code shuffling",
+          "timestamp": "2022-02-16T11:54:26+01:00",
+          "tree_id": "c4f6576921bbbac71da13b519fad56d10d67f0f6",
+          "url": "https://github.com/tilk/yieldfsm-riscv/commit/75b54d90070983d954a786265f29e89af2a3c338"
+        },
+        "date": 1645010830677,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "yieldfsm - fmax",
+            "value": 33.52,
+            "unit": "MHz"
+          },
+          {
+            "name": "explicitdp - fmax",
+            "value": 39.44,
             "unit": "MHz"
           },
           {
