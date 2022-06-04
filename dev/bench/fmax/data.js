48b83224b4bada8dce18cbb6e6f7b9b6ff2b3ec4
github-action-benchmark <github@users.noreply.github.com>
2022-06-04 09:23:12 +0000

    add Fmax (nextpnr, ice40) (customBiggerIsBetter) benchmark result for 349e0eeea9a1d819b8e0ccbea0730e230703bddd

--- dev/bench/fmax/data.js
+++ dev/bench/fmax/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1645270880022,
+  "lastUpdate": 1654334592277,
   "repoUrl": "https://github.com/tilk/yieldfsm-riscv",
   "entries": {
     "Fmax (nextpnr, ice40)": [
@@ -350,6 +350,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "explicit - fmax",
             "value": 39.59,
+            "unit": "MHz"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tilk@tilk.eu",
+            "name": "Marek Materzok",
+            "username": "tilk"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "349e0eeea9a1d819b8e0ccbea0730e230703bddd",
+          "message": "Update README.md",
+          "timestamp": "2022-06-04T10:50:23+02:00",
+          "tree_id": "9f93d3086bdb483ae569b1896e26bf75b1be42bb",
+          "url": "https://github.com/tilk/yieldfsm-riscv/commit/349e0eeea9a1d819b8e0ccbea0730e230703bddd"
+        },
+        "date": 1654334591140,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "explicit - fmax",
+            "value": 39.59,
+            "unit": "MHz"
+          },
+          {
+            "name": "explicitdp - fmax",
+            "value": 39.44,
+            "unit": "MHz"
+          },
+          {
+            "name": "yieldfsm - fmax",
+            "value": 33.52,
             "unit": "MHz"
           }
         ]
